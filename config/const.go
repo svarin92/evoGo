b482ae1e8be3f720946436427608c17c34cca2b3
svarin92 <svarin92@gmail.com>
2026-08-28 16:57:00 +0200

    Modification of evolution constants

--- config/const.go
+++ config/const.go
@@ -7,20 +7,22 @@ const (
 	CODONS_SIZE             int     = 127
 	CROSSOVER_PROBABILITY   float64 = 0.7
 	ELITE_SIZE              int     = 1
-	GENERATIONS             int     = 30
-	GENERATION_SIZE         int     = 50
+	GENERATIONS             int     = 10
+	GENERATION_SIZE         int     = 20
 
 	// MAX_WRAPS defines the maximum number of times the genome can be reused.
-	// A value of 100 allows for good diversity without the risk of infinite loops.
+	// A value of 100 allows for good diversity without the risk of infinite 
+	// loops.
 	// Note: A value of 0 disables bouncing (simple initial behavior).
 	MAX_WRAPS 			    int 	= 0
 
-	// Adjust the penalty factor. Too high a factor penalizes partial solutions too
-	// heavily. A factor that is too low does not sufficiently reduce wraps.
+	// Adjust the penalty factor. Too high a factor penalizes partial solutions 
+	// too heavily. A factor that is too low does not sufficiently reduce 
+	// wraps.
 	WRAP_PENALTY_FACTOR     float64 = 0.01
 
 	MUTATION_PROBABILITY    float64 = 0.01
-	POPULATION_SIZE         int     = 50
+	POPULATION_SIZE         int     = 20
 
 	EXPLORATION_PROBABILITY float64 = 0.2
 )
